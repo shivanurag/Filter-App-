25599928bf2d3ea058728f1688846d135ab8c4d3
shivanurag <77777785+shivanurag@users.noreply.github.com>
2023-04-20 22:48:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+ function preload()
+{
+
+}
+
+ function setup()
+{
+    canvas=createCanvas(900,900);
+    canvas.center()
+}
+
+ function draw()
+{
+
+}
+
+ function take_snapshot()
+{
+    save('Mustache face.png');
+}
